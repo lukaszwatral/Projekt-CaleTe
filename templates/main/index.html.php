07887bd82216440920765494df6175a389ae71b5
wl53674 <wl53674@zut.edu.pl>
2025-01-15 19:43:50 +0100

    Zmiany frontendowe, dodane cachowanie eventów w kalendarzu

--- templates/main/index.html.php
+++ templates/main/index.html.php
@@ -3,17 +3,20 @@
 /** @var \App\Model\Filter[] $filteredLessons */
 /** @var \App\Service\Router $router */
 
-$title = 'Filter';
+$title = 'Plan Całe Te';
 $bodyClass = 'index';
 
 ob_start(); ?>
-
+    <div id="history">
+        <button type="button" id="historyBtn" class="btn">HISTORIA</button>
+    </div>
     <div id="filterForm">
+        <div class="filterContainer">
         <form action="<?= $router->generatePath('main-index') ?>" method="get">
-            <label for="teacher">Wykladowca:</label><br>
+            <label for="teacher">Wykładowca:</label><br>
             <input type="text" id="teacher" name="teacher" value="<?= htmlspecialchars($_GET['teacher'] ?? '') ?>"><br>
-            <label for="room">Sala:</label><br>
-            <input type="text" id="room" name="room" value="<?= htmlspecialchars($_GET['classroom'] ?? '') ?>"><br>
+            <label for="classroom">Sala:</label><br>
+            <input type="text" id="classroom" name="classroom" value="<?= htmlspecialchars($_GET['classroom'] ?? '') ?>"><br>
             <label for="subject">Przedmiot:</label><br>
             <input type="text" id="subject" name="subject" value="<?= htmlspecialchars($_GET['subject'] ?? '') ?>"><br>
             <label for="studyGroup">Grupa:</label><br>
@@ -22,15 +25,15 @@ ob_start(); ?>
             <input type="text" id="student" name="student" value="<?= htmlspecialchars($_GET['student'] ?? '') ?>"><br>
 
             <div id="advanced-filters" style="display: none;">
-                <label for="department">Wydzial:</label><br>
+                <label for="department">Wydział:</label><br>
                 <input type="text" id="department" name="department" value="<?= htmlspecialchars($_GET['department'] ?? '') ?>"><br>
                 <label for="subjectForm">Forma przedmiotu:</label><br>
                 <input type="text" id="subjectForm" name="subjectForm" value="<?= htmlspecialchars($_GET['subjectForm'] ?? '') ?>"><br>
-                <label for="studyCourse">Typ studiow:</label><br>
+                <label for="studyCourse">Tok studiów:</label><br>
                 <input type="text" id="studyCourse" name="studyCourse" value="<?= htmlspecialchars($_GET['studyCourse'] ?? '') ?>"><br>
                 <label for="semester">Semestr:</label><br>
                 <input type="text" id="semester" name="semester" value="<?= htmlspecialchars($_GET['semester'] ?? '') ?>"><br>
-                <label for="yearOfStudy">Rok studiow:</label><br>
+                <label for="yearOfStudy">Rok studiów:</label><br>
                 <input type="text" id="yearOfStudy" name="yearOfStudy" value="<?= htmlspecialchars($_GET['yearOfStudy'] ?? '') ?>"><br>
                 <label for="major">Kierunek:</label><br>
                 <input type="text" id="major" name="major" value="<?= htmlspecialchars($_GET['major'] ?? '') ?>"><br>
@@ -39,14 +42,14 @@ ob_start(); ?>
             </div>
 
             <button type="button" id="toggle-advanced-filters" class="btn">Filtry zaawansowane</button>
-            <button type="submit" value="Submit" class="btn">Filter</button>
         </form>
         <div class="button-container">
             <button type="button" id="search-btn" class="btn">Szukaj</button>
             <button type="button" id="reset-filters" class="btn">Wyczyść filtry</button>
         </div>
+        </div>
     </div>
-    <script src="/public/assets/scripts/filter.js"></script>
+    <script src="/assets/scripts/filter.js"></script>
 
 
 
@@ -71,12 +74,12 @@ ob_start(); ?>
         <?php include __DIR__ . '/../filter/_form.html.php'; ?>
     </form>
 
-    <link href="/public/assets/src/less/_calendar.css" rel="stylesheet" />
+<!--    <link href="/public/assets/src/less/_calendar.css" rel="stylesheet" />-->
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
     <!-- Include FullCalendar CSS from node_modules -->
-    <link href="../../node_modules/@fullcalendar/main.min.css" rel="stylesheet" />
+<!--    <link href="../../node_modules/@fullcalendar/main.min.css" rel="stylesheet" />-->
     <!-- Include tippy.js CSS -->
-    <link href="https://unpkg.com/tippy.js@6/dist/tippy.css" rel="stylesheet" />
+<!--    <link href="https://unpkg.com/tippy.js@6/dist/tippy.css" rel="stylesheet" />-->
     <!-- Create a container for the calendar -->
     <div id="calendar"></div>
     <div id="legend">
@@ -91,36 +94,13 @@ ob_start(); ?>
     </div>
 
     <!-- Include FullCalendar JS from node_modules -->
-    <script src="../../node_modules/@fullcalendar/main.min.js"></script>
+<!--    <script src="../../node_modules/@fullcalendar/main.min.js"></script>-->
     <!-- Include tippy.js -->
     <script src="https://unpkg.com/tippy.js@6"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             var calendarEl = document.getElementById('calendar');
-            var events = [];
-            var currentDates = { start: '', end: '' };
-            async function fetchEvents(startDate, endDate) {
-                console.log(`Fetching events from ${startDate} to ${endDate}`);
-                try {
-                    const response = await fetch(url);
-                    if (!response.ok) throw new Error('Failed to fetch events. Status: ' + response.status);
-
-                    const data = await response.json();
-                    console.log('Fetched events:', data);
-
-                    events = data.map(event => ({
-                        title: event.title,
-                        start: event.start,
-                        end: event.end,
-                        description: event.description,
-                        color: event.color,
-                        extendedProps: event.extendedProps
-                    }));
-                } catch (error) {
-                    console.error('Error fetching events:', error);
-                }
-            }
-
+            var eventsCache = {};
             var customEvent = [
                 {
                     title: 'Sieci komputerowe (L)',
@@ -165,21 +145,46 @@ ob_start(); ?>
                 }
             ];
 
+            async function fetchEvents(startDate, endDate) {
+                const cacheKey = `${startDate}_${endDate}`;
+                if (eventsCache[cacheKey]) {
+                    return eventsCache[cacheKey];
+                }
+
+                console.log(`Fetching events from ${startDate} to ${endDate}`);
+                try {
+                    const response = await fetch(url);
+                    if (!response.ok) throw new Error('Failed to fetch events. Status: ' + response.status);
+
+                    const data = await response.json();
+                    console.log('Fetched events:', data);
+
+                    const events = data.map(event => ({
+                        title: event.title,
+                        start: event.start,
+                        end: event.end,
+                        description: event.description,
+                        color: event.color,
+                        extendedProps: event.extendedProps
+                    }));
+
+                    eventsCache[cacheKey] = events;
+                    return events;
+                } catch (error) {
+                    console.error('Error fetching events:', error);
+                    return [];
+                }
+            }
+
             var calendar = new FullCalendar.Calendar(calendarEl, {
                 initialView: 'timeGridWeek',
                 firstDay: 1,
                 events: async function (fetchInfo, successCallback, failureCallback) {
                     const { startStr, endStr } = fetchInfo;
-                    currentDates.start = startStr;
-                    currentDates.end = endStr;
-
-                    console.log(`Fetching events from ${startStr} to ${endStr}`);
-
-                    await fetchEvents(startStr, endStr);
-                    events = events.concat(customEvent);
-                    if (events.length > 0) {
-                        successCallback(events);
-                    } else {
+                    try {
+                        const events = await fetchEvents(startStr, endStr);
+                        successCallback(events.concat(customEvent));
+                    } catch (error) {
                         failureCallback('No events fetched');
                     }
                 },
@@ -209,13 +214,9 @@ ob_start(); ?>
                 },
                 locale: 'pl',
                 allDaySlot: false,
-
-                datesSet: (dateInfo) => {
-                    console.log(`Date range changed: ${dateInfo.startStr} to ${dateInfo.endStr}`);
-                    currentDates.start = dateInfo.startStr;
-                    currentDates.end = dateInfo.endStr;
-                    fetchEvents(dateInfo.startStr, dateInfo.endStr);
-                },
+                // datesSet: (dateInfo) => {
+                //     console.log(`Date range changed: ${dateInfo.startStr} to ${dateInfo.endStr}`);
+                // },
             });
 
             calendar.render();
